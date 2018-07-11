d6ef896ea7ac44c600942eb6e86224c30c1d408a
672099082 <672099082@qq.com>
2018-07-11 17:06:05 +0800

    789789

---
+++ src/main/java/org/java/spring/Springboot03outconfigApplication.java
@@ -0,0 +1,12 @@
+package org.java.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot03outconfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot03outconfigApplication.class, args);
+	}
+}
